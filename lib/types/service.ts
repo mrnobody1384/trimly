f8dba469d5327bfd779edf62f191f0171eab048e
VaryoSc <waryor99@gmail.com>
2025-01-07 00:05:17 +0330

    Asissted merging🤝

--- lib/types/service.ts
+++
@@ -1,7 +0,0 @@
-export interface service {
-  id?: string;
-  name: string;
-  description: string;
-  price: number;
-  duration: any;
-}
--- lib/types/user.ts
+++
@@ -1,7 +0,0 @@
-export interface UserType {
-  first_name: string;
-  last_name: string;
-  email: string;
-  phone_number?: string;
-  image_profile: string | null;
-}
